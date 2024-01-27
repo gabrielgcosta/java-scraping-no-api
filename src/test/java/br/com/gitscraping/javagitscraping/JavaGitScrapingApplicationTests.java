94964b7bebf95c66bd363cde07cb4ff1e0755fc9
gabrielgcosta <gabriel.costa@codenapp.com>
2024-01-26 21:46:34 -0300

    creating automated test

--- src/test/java/br/com/gitscraping/javagitscraping/JavaGitScrapingApplicationTests.java
+++ src/test/java/br/com/gitscraping/javagitscraping/JavaGitScrapingApplicationTests.java
@@ -1,13 +1,39 @@
 package br.com.gitscraping.javagitscraping;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class JavaGitScrapingApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    public void ApiTest() {
+        // Configure a base URI para suas chamadas de API
+        RestAssured.baseURI = "http://localhost:8080";
+
+        // Faça a solicitação HTTP GET para a rota desejada
+        Response response = given()
+                .param("rep", "gabrielgcosta/botcopa")
+                .when()
+                .get("/git-info");
+
+        // Verifique se o status da resposta é 200
+        assertEquals(200, response.getStatusCode());
+		assertTrue(response.getBody().asString().contains("extension"));
+		assertTrue(response.getBody().asString().contains("count"));
+		assertTrue(response.getBody().asString().contains("lines"));
+		assertTrue(response.getBody().asString().contains("bytes"));
+
+
+    }
 
 }
