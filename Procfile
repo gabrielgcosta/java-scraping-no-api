95974f34768eff82b0aa91867c503dd0cedf47d0
gabrielgcosta <gabriel.costa@codenapp.com>
2024-01-28 12:11:50 -0300

    deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=8080 $JAVA_OPTS -jar target/java-git-scraping-0.0.1-SNAPSHOT.jar
+web: java $JAVA_OPTS -Dserver.port=$PORT -jar target/java-git-scraping-0.0.1-SNAPSHOT.jar
--- src/main/java/br/com/gitscraping/javagitscraping/JavaGitScrapingApplication.java
+++ src/main/java/br/com/gitscraping/javagitscraping/JavaGitScrapingApplication.java
@@ -2,9 +2,15 @@ package br.com.gitscraping.javagitscraping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JavaGitScrapingApplication {
+
+	@GetMapping("/")
+	public String teste(){
+		return "teste";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaGitScrapingApplication.class, args);
